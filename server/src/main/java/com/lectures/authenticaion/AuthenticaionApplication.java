0f52c8c9957c50c875eef627567cbd0cc65eb840
Alphanumeric Str <150432237+numericar@users.noreply.github.com>
2025-04-07 16:01:31 +0700

    initial project

---
+++ server/src/main/java/com/lectures/authenticaion/AuthenticaionApplication.java
@@ -0,0 +1,13 @@
+package com.lectures.authenticaion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticaionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticaionApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/lectures/authenticaion/AuthenticaionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lectures.authenticaion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticaionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
